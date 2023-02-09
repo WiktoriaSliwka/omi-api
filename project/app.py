e3d79e9234f281de108aa714a2a5bf72e51e8d5f
aiyamadeline <aiyaedwards123@gmail.com>
2023-02-09 09:53:12 +0000

    created endpoints

---
+++ project/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import json
+import sqlite3
+from flask_restful import Api, Resource
+import requests
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route("/products", methods=['GET'])
+def products():
+    req = requests.get('https://api.storerestapi.com/products')
+    return (req.content)
+
+@app.route("/")
+def home():
+    return "Home page"
+
+# @app.route("/users")
+# def log_in():
+#     return "log in page"
+
+# @app.route("/register", methods=['GET'])
+# def register():  
+#     return 
+
+#@app.route("/delete", methods=['DELETE'])
+
+
+# python -m flask run (to run server, make sure cd into project)
+if __name__ == '__main__':
+    app.run()
